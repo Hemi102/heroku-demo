14bf57dcc4dd172155736d67228305a912854c87
shazim-k <shazims000@gmail.com>
2023-05-31 16:56:29 +0500

    fixed phosphor components and fixed checkbox

--- src/components/questions/Actions.js
+++ src/components/questions/Actions.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import plusIcon from 'assets/images/plus-icon.svg';
-import searchIcon from 'assets/images/search.svg';
-import uploadIcon from 'assets/images/upload.svg';
-import './actions.scss';
+import { Plus, UploadSimple, MagnifyingGlass } from 'phosphor-react';
+
 
 const Actions = () => {
   return (
@@ -12,17 +10,17 @@ const Actions = () => {
           <div className='d-flex flex-wrap align-items-center justify-content-between'>
             <div className='d-flex mt-2'>
               <span>
-                <img className='search-icon ms-3' src={searchIcon} alt='' />
+                <MagnifyingGlass size={24} className='search-icon ms-3'/>
               </span>
               <input className='search-input flex-grow-1' type='search' placeholder='Search Questions'></input>
             </div>
             <div className='d-flex mt-2'>
               <button className='secendory-btn text-nowrap'>
-                <img className='me-3' src={uploadIcon} alt='' />
+                <UploadSimple size={24} className='me-3'/>
                 Upload File
               </button>
               <button className='sm-btn text-nowrap ms-2'>
-                <img className='me-3' src={plusIcon} alt='' />
+                <Plus size={24} className='me-3'/>
                 Add Question
               </button>
             </div>
--- src/components/questions/Questionslist.js
+++ src/components/questions/Questionslist.js
@@ -1,113 +1,124 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './questionlist.scss';
-import editIcon from 'assets/images/edit.svg';
-import delIcon from 'assets/images/delete.svg';
+import { Pencil, Trash } from 'phosphor-react';
+
 
 const Questionslist = () => {
+  const questions = [
+    {
+      text: 'What was the type of outreach?',
+      type: 'Multiple Choice',
+      dateCreated: 'Apr 3, 2023',
+      dateUpdated: 'Dec 10, 2023',
+    },
+    {
+      text: 'What was the duration of outreach?',
+      type: 'Dropdown',
+      dateCreated: 'Apr 3, 2023',
+      dateUpdated: 'Dec 10, 2023',
+    },
+    {
+      text: 'What was the type of outreach?',
+      type: 'Multiple Choice',
+      dateCreated: 'Apr 3, 2023',
+      dateUpdated: 'Dec 10, 2023',
+    },
+    {
+      text: 'What was the type of outreach?',
+      type: 'Multiple Choice',
+      dateCreated: 'Apr 3, 2023',
+      dateUpdated: 'Dec 10, 2023',
+    },
+    {
+      text: 'What was the type of outreach?',
+      type: 'Multiple Choice',
+      dateCreated: 'Apr 3, 2023',
+      dateUpdated: 'Dec 10, 2023',
+    },
+
+  ];
+
+  const [selectAll, setSelectAll] = useState(false);
+  const [checkboxes, setCheckboxes] = useState(questions.map((question) => ({
+    ...question,
+    checked: false,
+  })));
+
+  const handleSelectAll = () => {
+    const updatedCheckboxes = checkboxes.map((checkbox) => ({
+      ...checkbox,
+      checked: !selectAll,
+    }));
+    setCheckboxes(updatedCheckboxes);
+    setSelectAll(!selectAll);
+  };
+
+  const handleCheckboxChange = (index) => {
+    const updatedCheckboxes = [...checkboxes];
+    const checkbox = updatedCheckboxes[index];
+    if (checkbox) {
+      checkbox.checked = !checkbox.checked;
+      setCheckboxes(updatedCheckboxes);
+    }
+  };
+
+
+
   return (
     <div className="container-fluid py-4">
-        <div className="table-responsive">
-          <table className="table table-rows">
-            <thead className="table-header">
-              <tr>
-                <th>
-                  <div className="form-check ps-3 mb-0">
-                    <input className="form-check-input" type="checkbox" value=""/>
-                  </div>
-                </th>
-                <th>Question</th>
-                <th>Type</th>
-                <th>Status</th>
-                <th>Date Created</th>
-                <th>Date Update</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
+      <div className="table-responsive">
+        <table className="table table-rows">
+          <thead className="table-header">
+            <tr>
+              <th>
+                <div className="form-check ps-3 mb-0">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    checked={selectAll}
+                    onChange={handleSelectAll}
+                  />
+                </div>
+              </th>
+              <th>Question</th>
+              <th>Type</th>
+              <th>Status</th>
+              <th>Date Created</th>
+              <th>Date Update</th>
+            </tr>
+          </thead>
+          <tbody>
+            {checkboxes.map((checkbox, index) => (
+              <tr key={index}>
                 <td>
                   <div className="form-check ps-3 mb-0">
-                    <input className="form-check-input" type="checkbox" value=""/>
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      checked={checkbox.checked}
+                      onChange={() => handleCheckboxChange(index)}
+                      value=""
+                    />
                   </div>
                 </td>
-                <td>What was the type of outreach?</td>
-                <td>Multiple Choice</td>
-                <td><div className="success-status">Active</div></td>
-                <td>Apr 3, 2023</td>
-                <td>Dec 10, 2023</td>
+                <td>{checkbox.text}</td>
+                <td>{checkbox.type}</td>
                 <td>
-                  <img className='opacity-50' src={editIcon} alt=''/>
-                  <img className='ms-3 opacity-50' src={delIcon} alt=''/>
+                  <div className="success-status">{checkbox.status}</div>
+                </td>
+                <td>{checkbox.dateCreated}</td>
+                <td>{checkbox.dateUpdated}</td>
+                <td>
+                  <Pencil size={24} className="opacity-50" />
+                  <Trash size={24} className="ms-3 opacity-50" />
                 </td>
               </tr>
-              <tr>
-                <td>
-                  <div className="form-check ps-3 mb-0">
-                    <input className="form-check-input" type="checkbox" value=""/>
-                  </div>
-                </td>
-                <td>What was the duration of outreach?</td>
-                <td>Dropdown</td>
-                <td><div className="success-status">Active</div></td>
-                <td>Apr 3, 2023</td>
-                <td>Dec 10, 2023</td>
-                <td>
-                  <img className='opacity-50' src={editIcon} alt=''/>
-                  <img className='ms-3 opacity-50' src={delIcon} alt=''/>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="form-check ps-3 mb-0">
-                    <input className="form-check-input" type="checkbox" value=""/>
-                  </div>
-                </td>
-                <td>What was the type of outreach?</td>
-                <td>Multiple Choice</td>
-                <td><div className="success-status">Active</div></td>
-                <td>Apr 3, 2023</td>
-                <td>Dec 10, 2023</td>
-                <td>
-                  <img className='opacity-50' src={editIcon} alt=''/>
-                  <img className='ms-3 opacity-50' src={delIcon} alt=''/>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="form-check ps-3 mb-0">
-                    <input className="form-check-input" type="checkbox" value=""/>
-                  </div>
-                </td>
-                <td>What was the type of outreach?</td>
-                <td>Multiple Choice</td>
-                <td><div className="success-status">Active</div></td>
-                <td>Apr 3, 2023</td>
-                <td>Dec 10, 2023</td>
-                <td>
-                  <img className='opacity-50' src={editIcon} alt=''/>
-                  <img className='ms-3 opacity-50' src={delIcon} alt=''/>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="form-check me-2 ps-3 mb-0">
-                    <input className="form-check-input" type="checkbox" value=""/>
-                  </div>
-                </td>
-                <td>What was the type of outreach?</td>
-                <td>Multiple Choice</td>
-                <td><div className="success-status">Active</div></td>
-                <td>Apr 3, 2023</td>
-                <td>Dec 10, 2023</td>
-                <td>
-                  <img className='opacity-50' src={editIcon} alt=''/>
-                  <img className='ms-3 opacity-50' src={delIcon} alt=''/>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          </div>
+            ))}
+          </tbody>
+        </table>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Questionslist
+export default Questionslist;

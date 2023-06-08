c7763baa69b99cb493db18f9b3fe8fa059259a9a
shazim-k <shazims000@gmail.com>
2023-06-08 18:07:39 +0500

    fixed all routs in sidebar

--- src/constants/routePaths.js
+++ src/constants/routePaths.js
@@ -1,3 +1,7 @@
 export const LOGIN_PATH = '/';
 export const DASHBOARD_PATH = '/dashboard';
 export const QUESTIONS_PATH = '/questions';
+export const Leadcaremanager_PATH = '/leadcaremanager';
+export const OutreachLeaders_PATH = '/outreachLeaders';
+export const OutreachStaff_PATH = '/outreachstaff';
+export const Members_PATH = '/members';

e7dfbb6919cb6836e902418cf1a260ce33766fe6
shazim-k <shazims000@gmail.com>
2023-06-14 18:30:15 +0500

    make lead care manager page and modal

---
+++ src/components/caremanager/Addmanagerform.js
@@ -0,0 +1,119 @@
+import Input from 'components/common/input';
+import { ErrorMessage, Formik } from 'formik';
+import React from 'react';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  careCoordinatorName: yup.string().required(),
+  email: yup.string().email().required(),
+  location: yup.string().required(),
+  capacitylimit: yup.number().required().positive().integer(),
+});
+
+
+const AddQuestionForm = ({ handleQuestionSubmittion, handleClose }) => {
+  return (
+    <Formik
+      initialValues={{
+        name: '',
+        careCoordinatorName: '',
+        email: '',
+        location: '',
+        capacitylimit: '',
+      }}
+      validationSchema={schema}
+      onSubmit={values => {
+        handleQuestionSubmittion(values);
+      }}
+    >
+      {({ handleChange, values, handleSubmit, errors }) => {
+        const customHandleChange = (event) => {
+          const { name, value } = event.target;
+          if (name === 'capacitylimit') {
+            const parsedValue = parseInt(value, 10);
+            handleChange({
+              target: {
+                name,
+                value: isNaN(parsedValue) ? '' : parsedValue,
+              },
+            });
+          } else {
+            handleChange(event);
+          }
+        };
+        console.log('errors are', errors);
+        console.log('values are', values);
+        return (
+          <div className="question-modal">
+            <form className="form-main" onSubmit={handleSubmit}>
+              <div className="d-flex justify-content-between">
+                <Input
+                  handleChange={handleChange}
+                  placeholder="Name"
+                  type="text"
+                  label="Lead Care manager Name"
+                  value={values.leadCareManagerName}
+                  name="name"
+                />
+                <ErrorMessage component="p" name="name" />
+                <Input
+                  handleChange={handleChange}
+                  placeholder="Name"
+                  type="text"
+                  label="Care coordinator Name"
+                  value={values.careCoordinatorName}
+                  name="careCoordinatorName"
+                />
+                <ErrorMessage component="p" name="careCoordinatorName" />
+              </div>
+              <div>
+                <Input
+                  handleChange={handleChange}
+                  placeholder="Email"
+                  type="email"
+                  label="Email"
+                  value={values.email}
+                  name="email"
+                />
+                <ErrorMessage component="p" name="email" />
+              </div>
+              <div>
+                <Input
+                  handleChange={handleChange}
+                  placeholder="Location"
+                  type="text"
+                  label="Location(s)"
+                  value={values.location}
+                  name="location"
+                />
+                <ErrorMessage component="p" name="location" />
+              </div>
+              <div>
+                <Input
+                  handleChange={customHandleChange}
+                  placeholder="Capacity Limit"
+                  type="text"
+                  label="Capacity Limit"
+                  value={values.capacitylimit !== '' ? `${values.capacitylimit}` : ''}
+                  name="capacitylimit"
+                />
+
+                <ErrorMessage component="p" name="capacitylimit" />
+              </div>
+
+              <div className="action gap-2">
+                <button className="secondary-btn w-50" type="button" onClick={handleClose}>
+                  Cancel
+                </button>
+                <input className="primary-btn w-50" type="submit" name="submit" value="Submit" />
+              </div>
+            </form>
+          </div>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default AddQuestionForm;
---
+++ src/components/caremanager/Leadcaremanagers.js
@@ -0,0 +1,226 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import {Pencil, Plus, Trash, TrashSimple, UploadSimple} from 'phosphor-react';
+import TableWrapper from 'components/common/table-wrapper';
+import {DEBOUNCE_DELAY, initialMetaForTable} from 'constants/common';
+import {debounce} from 'lodash';
+import CustomModal from 'components/common/modal';
+import Addmanagerform from './Addmanagerform';
+
+const Leadcaremanagers = () => {
+  const [managersList, setManagersList] = useState([
+    {
+      name: 'Watson Jhonson',
+      email: 'watsonjhonson@gamil.com',
+      location: 'Titanium Northren California',
+      capacitylimit: '5/49',
+      dateCreated: 'Apr 3, 2023',
+      checked: false,
+    },
+    {
+      name: 'Watson Jhonson',
+      email: 'watsonjhonson@gamil.com',
+      location: 'Titanium Northren California',
+      capacitylimit: '5/49',
+      dateCreated: 'Apr 3, 2023',
+      checked: false,
+    },
+    {
+      name: 'Watson Jhonson',
+      email: 'watsonjhonson@gamil.com',
+      location: 'Titanium Northren California',
+      capacitylimit: '5/49',
+      dateCreated: 'Apr 3, 2023',
+      checked: false,
+    }
+
+  ]);
+ 
+   const MAX_CAPACITY=49;
+  
+
+  const [selectAll, setSelectAll] = useState(false);
+  const [meta, setMeta] = useState(initialMetaForTable);
+  const [loading, setLoading] = useState(true);
+  const [selectedManagers, setSelectedManagers] = useState(0);
+  const [isManagerModalVisible, setIsManagerModalVisible] = useState(false);
+
+  const debounceFn = useCallback(
+    debounce(() => {
+      setLoading(true);
+    }, DEBOUNCE_DELAY),
+    [meta.search]
+  );
+
+  const handleSetSearchQuery = (value) => {
+    setMeta((prevMeta) => ({ ...prevMeta, search: value }));
+    debounceFn();
+  };
+
+  const handlePageChange = (value) => {
+    setMeta((prevMeta) => ({ ...prevMeta, page: value }));
+    setLoading(true);
+  };
+
+  const handleManagerSubmission = (manager) => {
+    setManagersList((prevList) => [
+      ...prevList,
+      {
+        name: manager.name,
+        careCoordinatorName: manager.careCoordinatorName, 
+        email: manager.email,
+        location: manager.location,
+        capacitylimit: `${manager.capacitylimit}/${MAX_CAPACITY}`,
+        dateCreated: 'Apr 3, 2023',
+        checked: false,
+      },
+    ]);
+    handleCloseManagerModal();
+  };
+
+  const handleSelectAll = () => {
+    const updatedCheckboxes = managersList.map((checkbox) => ({
+      ...checkbox,
+      checked: !selectAll,
+    }));
+    setManagersList(updatedCheckboxes);
+    setSelectAll(!selectAll);
+  };
+
+  const handleCheckboxChange = (index) => {
+    setManagersList((prevList) =>
+      prevList.map((item, itemIndex) =>
+        itemIndex === index ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
+
+  const handleCloseManagerModal = () => {
+    setIsManagerModalVisible(false);
+  };
+  const handleOpenManagerModal = () => {
+    setIsManagerModalVisible(true);
+  };
+  
+
+  useEffect(() => {
+    setSelectedManagers(
+      selectAll
+        ? managersList.length
+        : managersList.reduce((accumulator, item) => {
+            if (item.checked) {
+              return accumulator + 1;
+            } else return accumulator;
+          }, 0)
+    );
+  }, [managersList, selectAll]);
+
+  return (
+    <>
+      <TableWrapper
+        searchPlaceholder="Search Lead Care Managers"
+        setSearhQuery={handleSetSearchQuery}
+        searchValue={meta.search}
+        totalListCount={100}
+        pageSize={meta.perPage}
+        currentPage={meta.page}
+        onPageChange={handlePageChange}
+        selectedItems={selectedManagers}
+        actionButtons={
+          selectedManagers > 0
+            ? [
+                {
+                  label: `Delete ${selectedManagers} Items`,
+                  classes: 'danger-btn',
+                  icon: <TrashSimple size={24} className="me-3" />,
+                  handleClick: () => {
+                   
+                  },
+                },
+              ]
+            : [
+                {
+                  label: 'Upload File',
+                  classes: 'secondary-btn',
+                  icon: <UploadSimple size={24} className="me-3" />,
+                  handleClick: () => {
+               
+                  },
+                },
+                {
+                  label: 'Add Lead Care Manager',
+                  classes: 'primary-btn',
+                  icon: <Plus size={24} className="me-3" />,
+                  handleClick: () => {
+                    console.log('clicked');
+                    
+                    handleOpenManagerModal();
+                  },
+                },
+              ]
+        }
+      >
+        <div className="container-fluid py-4">
+          <div className="table-responsive">
+            <table className="table table-rows">
+              <thead className="table-header">
+                <tr>
+                  <th>
+                    <div className="form-check ps-3 mb-0">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        checked={selectAll}
+                        onChange={handleSelectAll}
+                      />
+                    </div>
+                  </th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Location</th>
+                  <th>Capacity Limit</th>
+                  <th>Date Created</th>
+                </tr>
+              </thead>
+              <tbody>
+                {managersList.map((manager, index) => (
+                  <tr key={index}>
+                    <td>
+                      <div className="form-check ps-3 mb-0">
+                        <input
+                          className="form-check-input"
+                          type="checkbox"
+                          checked={manager.checked}
+                          onChange={() => handleCheckboxChange(index)}
+                          value=""
+                        />
+                      </div>
+                    </td>
+                    <td>
+                      {manager.name} , {manager.careCoordinatorName}
+                    </td>
+                    <td>{manager.email}</td>
+                    <td>{manager.location}</td>
+                    <td>{`${manager.capacitylimit}`}</td>
+                    <td>{manager.dateCreated}</td>
+           
+                    <td>
+                      <Pencil size={24} className="opacity-50" />
+                      <Trash size={24} className="ms-3 opacity-50" />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </TableWrapper>
+      {isManagerModalVisible && (
+        <CustomModal size="sm" show onHide={handleCloseManagerModal} heading="Add Question">
+          <Addmanagerform handleQuestionSubmittion={handleManagerSubmission} handleClose={handleCloseManagerModal} />
+        </CustomModal>
+      )}
+    </>
+  );
+};
+
+export default Leadcaremanagers;
